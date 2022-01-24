a78e92c241638722fcd7ea8ec9085df5f205a0fc
Tom Brien <TomBrien@users.noreply.github.com>
2022-01-24 15:24:02 +0000

    Reconfigure config flow to use post code search (#14)
    * Use update coordinator
    
    * Sort imports
    
    * Remove unused Throttle import
    
    * Remove unused imports
    
    * Add address lookup
    
    * Update readme for address search
    
    * Standardise schema naming

--- custom_components/cardiffwaste/config_flow.py
+++ custom_components/cardiffwaste/config_flow.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from cardiffwaste import WasteCollections
+from cardiffwaste import EmptyMatches
+from cardiffwaste import Timeout as WasteTimeout
+from cardiffwaste import WasteCollections, address_search
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -13,6 +15,8 @@ from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
 from .const import (
+    CONF_ADDRESS_PICKER,
+    CONF_POST_CODE,
     CONF_UPRN,
     DEFAULT_OPTIONS,
     DOMAIN,
@@ -26,21 +30,20 @@ from .helpers import redact_uprn
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_UPRN): str})
+STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_POST_CODE): str})
 
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Check we can get data for the property."""
 
-    _LOGGER.debug("Validating uprn: %s", redact_uprn(data[CONF_UPRN]))
+    _LOGGER.debug("Validating uprn: %s", redact_uprn(data[CONF_ADDRESS_PICKER]))
 
-    client = await hass.async_add_executor_job(WasteCollections, data[CONF_UPRN])
+    client = await hass.async_add_executor_job(
+        WasteCollections, data[CONF_ADDRESS_PICKER]
+    )
 
     if not client.check_valid_uprn():
         raise InvalidUPRN
-
-    # Return info that you want to store in the config entry.
-    return {"title": data[CONF_UPRN]}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -50,32 +53,78 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> config_entries.ConfigFlow:
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
                 step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
-        self._async_abort_entries_match({CONF_UPRN: user_input[CONF_UPRN]})
+        errors = {}
+
+        try:
+            self.matches = await self.hass.async_add_executor_job(
+                address_search, user_input[CONF_POST_CODE]
+            )
+        except WasteTimeout:
+            _LOGGER.debug("Timed out trying to lookup address")
+            errors["base"] = "timeout"
+        except EmptyMatches:
+            _LOGGER.debug("No matches for address search")
+            errors["base"] = "no_match"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return await self.async_step_address_picker()
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_address_picker(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle picking from the results."""
+
+        step_address_picker_schema = vol.Schema(
+            {vol.Required(CONF_ADDRESS_PICKER): vol.In(self.matches)},
+        )
+
+        if user_input is None:
+            return self.async_show_form(
+                step_id="address_picker", data_schema=step_address_picker_schema
+            )
 
         errors = {}
 
         try:
-            info = await validate_input(self.hass, user_input)
+            await validate_input(self.hass, user_input)
         except InvalidUPRN:
-            _LOGGER.debug("uprn: %s is invalid", redact_uprn(user_input[CONF_UPRN]))
+            _LOGGER.debug(
+                "uprn: %s is invalid", redact_uprn(user_input[CONF_ADDRESS_PICKER])
+            )
             errors["base"] = "invalid_uprn"
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception("Unexpected exception")
             errors["base"] = "unknown"
         else:
+
+            self._async_abort_entries_match(
+                {CONF_UPRN: user_input[CONF_ADDRESS_PICKER]}
+            )
+
+            title = self.matches[user_input[CONF_ADDRESS_PICKER]].split(",")[0]
+            data = {CONF_UPRN: user_input[CONF_ADDRESS_PICKER]}
+
             return self.async_create_entry(
-                title=info["title"], data=user_input, options=DEFAULT_OPTIONS
+                title=title, data=data, options=DEFAULT_OPTIONS
             )
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="address_picker",
+            data_schema=step_address_picker_schema,
+            errors=errors,
         )
 
     @staticmethod
